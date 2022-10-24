7cea18ac0116553974b3e653bfb99fbf75783411
sergiomonts <sergioblanco@iesmontsia.org>
2022-10-24 17:43:01 +0200

    Restruccturacion Repositorio

---
+++ Classes/PHP/fitxers_de_prova/ClassUsuari.php
@@ -0,0 +1,142 @@
+<?php
+
+class Usuari{
+    #Preparació dels atributs
+    private $id;
+    private $dni;
+    private $nom;
+    private $cognom;
+    private $telefon;
+    private $email;
+    private $insignies;
+    private $Nomusuaris;
+    private $PassUsuari;
+    private $TipusUsuari;
+    private $IdEmpresa;
+
+    #Preparació del constructor
+    public function __construct($id, $dni, $nom, $cognom, $telefon, $email, $insignies, $Nomusuaris,$TipusUsuari, $IdEmpresa ){
+    $this -> id = $id;
+    $this -> DNI = $dni;
+    $this -> nom = $nom;
+    $this -> cognom = $cognom;
+    $this -> email = $email;
+    $this -> insignies = $insignies;
+    $this -> NomUsuaris = $Nomusuaris;
+    $this -> TipusUsuari = $TipusUsuari; 
+    $this -> idEmpresa = $IdEmpresa;
+
+    }
+
+    #Preparació dels gets
+    public function getId(){
+        return $this -> id;
+    }
+    public function getdni(){
+        return $this -> DNI;
+    }
+    public function getNom(){
+        return $this -> Nom;
+    }
+    public function getCognom(){
+        return $this -> Cognom;
+    }
+    public function getEmail(){
+        return $this -> Email;
+    }
+    public function getInsignies(){
+        return $this -> Insignies;
+    }
+    public function getNomUsuaris(){
+        return $this -> NomUsuaris;
+    }
+    public function getTipusUsuari(){
+        return $this -> TipusUsuaris;
+    }
+    public function getIdEmpresa(){
+        return $this -> IdEmpresa;
+    }
+  #Preparació dels sets
+    public function setId($id){  
+        $this -> id = $id; 
+    }
+    public function setDni($dni){  
+        $this -> dni = $dni; 
+    }
+    public function setNom($nom){  
+        $this -> nom = $nom; 
+    }
+    public function setCognom($cognom){  
+        $this -> cognom = $cognom; 
+    }
+    public function setEmail($email){  
+        $this -> email = $email; 
+    }
+    public function setInsignies($insignies){  
+        $this -> insignies = $insignies; 
+    }
+    public function setNomUsuaris($Nomusuaris){  
+        $this -> NomUsuaris = $Nomusuaris; 
+    }
+    public function setTipusUsuari($TipusUsuari){  
+        $this -> TipusUsuari = $TipusUsuari; 
+    }
+    public function setIdEmpresa($IdEmpresa){  
+        $this -> IdEmpresa = $IdEmpresa; 
+    }
+    
+    #Preparació de les funcions
+    public function create($id, $dni, $nom, $cognom, $telefon, $email, $insignies, $Nomusuaris,$TipusUsuari, $IdEmpresa){
+        $this -> id = $id;
+        $this -> DNI = $dni;
+        $this -> nom = $nom;
+        $this -> cognom = $cognom;
+        $this -> email = $email;
+        $this -> insignies = $insignies;
+        $this -> NomUsuaris = $Nomusuaris;
+        $this -> TipusUsuari = $TipusUsuari; 
+        $this -> idEmpresa = $IdEmpresa;
+    
+    }
+    public function mostrar($id, $dni, $nom, $cognom, $telefon, $email, $insignies, $Nomusuaris,$TipusUsuari, $IdEmpresa){
+        $this -> id = $id;
+        $this -> DNI = $dni;
+        $this -> nom = $nom;
+        $this -> cognom = $cognom;
+        $this -> email = $email;
+        $this -> insignies = $insignies;
+        $this -> NomUsuaris = $Nomusuaris;
+        $this -> TipusUsuari = $TipusUsuari; 
+        $this -> idEmpresa = $IdEmpresa;
+    
+    }
+    public function update($id, $dni, $nom, $cognom, $telefon, $email, $insignies, $Nomusuaris,$TipusUsuari, $IdEmpresa){
+        $this -> id = $id;
+        $this -> DNI = $dni;
+        $this -> nom = $nom;
+        $this -> cognom = $cognom;
+        $this -> email = $email;
+        $this -> insignies = $insignies;
+        $this -> NomUsuaris = $Nomusuaris;
+        $this -> TipusUsuari = $TipusUsuari; 
+        $this -> idEmpresa = $IdEmpresa;
+    
+    }
+    public function delete($id, $dni, $nom, $cognom, $telefon, $email, $insignies, $Nomusuaris,$TipusUsuari, $IdEmpresa){
+        $this -> id = $id;
+        $this -> DNI = $dni;
+        $this -> nom = $nom;
+        $this -> cognom = $cognom;
+        $this -> email = $email;
+        $this -> insignies = $insignies;
+        $this -> NomUsuaris = $Nomusuaris;
+        $this -> TipusUsuari = $TipusUsuari; 
+        $this -> idEmpresa = $IdEmpresa;
+    
+    }
+    public function sendConfirmationMail($email){
+        $this -> email = $email;    
+    }
+}
+
+?>
---
+++ README.md
@@ -0,0 +1,21 @@
+# SPRINT1_GRUP1_Projecte_Globalitzat
+Repositori del primer sprint del Projecte Globalitzat, grup 1, DAW2. 
+
+## Què hi ha en aquest repositori?
+- Classes -> aquest directori conté les diferents classes necessàries per al nostre equip (en PHP)
+- HTML -> conté els diferents documents HTML, juntament amb els CSS necessaris per funcionar
+- Web -> directori de prova, és on dirigeix l'Apache del Docker
+- assets -> directori que conté els diferents elements més gràfics (logo i fons del login). De moment, són .svg, documents vectorials i escalables sense límit.
+- css -> conté els CSS comuns per les diferents pàgines (header, navbar i footer). D'aquesta manera, tenim estils comuns (a l'espera de Vue i Sass)
+- demo_img -> imatges de prova, guardades per al prototip (imatge del robot que apareix com a foto de perfil, per exemple)
+- json -> conté l'exemple de JSON del que volem recuperar de l'API
+- wireframes -> imatge dels wireframes que tenim
+
+## Què hem utilitzat?
+- Tipografia: Montserrat
+`@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');` -> al CSS
+- Icones: FontAwesome 
+`<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />` -> HTML
+
+## Finalment...
+Molta sort! Això és el que hem fet fins ara. Qualsevol dubte, pregunteu!
