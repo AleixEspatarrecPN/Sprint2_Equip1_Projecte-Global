1fd88b5dfbd194135b885da8209bd1a0f01cad1c
JuliaK18 <juliakrukonis@iesmontsia.org>
2022-11-08 20:03:41 +0100

    Plantilla (Consensuat amb tots els equips) KivyMD

--- KivyMD/PymeshieldAPP/sources/LlistatUsr/__usrList__.py
+++ KivyMD/PymeshieldAPP/sources/LlistatUsr/__usrList__.py
@@ -1,2 +1,28 @@
-#Ajuda per aquest apartat
-#https://www.youtube.com/watch?v=HMnAg_izNcY&list=PLhTjy8cBISEoQQLZ9IBlVlr4WjVoStmy-&index=10&ab_channel=buildwithpython
+from turtle import color
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import MDList
+
+class ContentNavigationDrawer(MDBoxLayout):
+    pass    
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+        
+
+class MyApp (MDApp):    
+    def build(self):
+        self.title = "PymeShield"
+        return Builder.load_file("main2.kv")
+
+
+MyApp().run()
