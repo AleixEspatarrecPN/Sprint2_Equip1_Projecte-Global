53717f501794fcc248ac28c2e8e7a91ff56f4c3a
sergiomonts <sergioblanco@iesmontsia.org>
2022-11-15 20:25:26 +0100

    modal contrasenya sm

--- DocumentRoot/html/infoPerfil/perfil.php
+++ DocumentRoot/html/infoPerfil/perfil.php
@@ -159,7 +159,7 @@
 </div>
 
 <div class="modal fade" id="modal-contrasenya" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Canvia la Contrasenya</h1>
