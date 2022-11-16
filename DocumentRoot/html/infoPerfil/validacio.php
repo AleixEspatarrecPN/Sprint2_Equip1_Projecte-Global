354f6c1b29401a8f91342ca07019a23fb93d9ea7
Aleix <aleixpaga@iesmontsia.org>
2022-11-16 21:26:08 +0100

    final_classe

--- DocumentRoot/html/infoPerfil/validacio.php
+++ DocumentRoot/html/infoPerfil/validacio.php
@@ -1,9 +1,10 @@
 <?php
-    include_once '../../proves_php/prova_login.php';
+    include_once '../../proves_php/Aleix_ClassUsuari.php';
 
     $passOld = $_POST['passCurrent'];
     $passNew = $_POST['passNew'];
     $passNewConf = $_POST['passNewConfirmation'];
     $changePass = new User();
     $changePass->changePass($passOld,$passNew,$passNewConf);
+    
 ?>
--- DocumentRoot/html/login/validar.php
+++ DocumentRoot/html/login/validar.php
@@ -1,5 +1,5 @@
 <?php
-    include_once '../../proves_php/prova_login.php';
+    include_once '../../proves_php/Aleix_ClassUsuari.php';
 
     $login = $_POST[''];
     $pass = $_POST['pass'];
