359d81e0c5124fa729d9e36142ec8a0ea4cd8217
janalmudeve <joanpasqualalmudeve@iesmontsia.org>
2022-11-19 15:12:05 +0100

    funcio validar contrasenya funciona

--- DocumentRoot/html/infoPerfil/perfil.php
+++ DocumentRoot/html/infoPerfil/perfil.php
@@ -173,6 +173,31 @@
             </div>
             <div class="modal-body">
                 <form action="./validacio.php" method="post" id=form>
+                <script>
+                    function validar(tx) 
+                    { 
+                        var nMay = 0, nMin = 0, nNum = 0 
+                        var t1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+                        var t2 = "abcdefghijklmnopqrstuvwxyz" 
+                        var t3 = "0123456789"
+                            if (tx.length < 8) {
+                                        alert("La contrasenya, ha de tenir almenys 8 lletres");
+                            } if (tx.length > 50) {
+                                        alert("La contrasenya, ha de tenir menys de 50 lletres");
+                            } else {
+                                    //Aqui continua si la variable ya tiene mas de 5 letras
+                            for (i=0;i<tx.length;i++) { 
+                                if ( t1.indexOf(tx.charAt(i)) != -1 ) {nMay++} 
+                                if ( t2.indexOf(tx.charAt(i)) != -1 ) {nMin++} 
+                                if ( t3.indexOf(tx.charAt(i)) != -1 ) {nNum++} 
+                            } 
+                        if ( nMay>0 && nMin>0 && nNum>0 ) 
+                        form.submit()
+                        else 
+                        { alert("La seva contrasenya a de contenir almenys 5 caracters alfanumerics"); form.passNew.focus(); return; }
+                        }
+                    }
+                </script>
                     <div class="mb-3">
                         <label for="recipient-name" class="col-form-label">Contrasenya Actual:</label>
                         <input type="password" name="passCurrent" class="form-control" id="recipient-actual-password" required maxlength="50" required minlength="8">
@@ -185,9 +210,6 @@
                         <label for="message-text" class="col-form-label">Confirma la Contrasenya:</label>
                         <input type="password" nem="passNewConfirmation" class="form-control" id="new-password-confirmation" required maxlength="50" required minlength="8">
                     </div>
-                        <span role="alert" id="passError" aria-hidden="true">
-                            Perfavor ingresa la contrasenya.
-                        </span>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tanca</button>
