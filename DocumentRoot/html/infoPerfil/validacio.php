c07b17dd2a293f073129b94730fb8fc8aac9c5db
JuliaK18 <juliakrukonis@iesmontsia.org>
2022-11-20 18:24:29 +0100

    Actualització de la classe

--- DocumentRoot/html/infoPerfil/validacio.php
+++ DocumentRoot/html/infoPerfil/validacio.php
@@ -4,6 +4,7 @@
     $passOld = $_POST['passCurrent'];
     $passNew = $_POST['passNew'];
     $passNewConf = $_POST['passNewConfirmation'];
+    
     $changePass = new User();
     $changePass->changePass($passOld,$passNew,$passNewConf);    
 ?>
