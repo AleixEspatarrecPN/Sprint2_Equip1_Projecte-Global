08e2990519d4d455fc99fbae1daf1fe54b5dd205
sergiomonts <sergioblanco@iesmontsia.org>
2022-11-22 00:46:49 +0100

    idioma

--- DocumentRoot/html/llistatUsuaris/index.php
+++ DocumentRoot/html/llistatUsuaris/index.php
@@ -161,7 +161,7 @@ include_once "../../php/ClassUsuari.php"
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"> Usuarios Deshabilitados </h5>
+                <h5 class="modal-title" id="exampleModalLabel"> Usuarios Deshabilitados</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="d-flex justify-content-around">
